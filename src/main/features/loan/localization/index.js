463c4d6e63e4636eeecb92d0a7ae73e02c22a514
Amir Naveed <amir@miletap.com>
2022-06-03 20:19:26 +0500

    Loan initial composer

--- src/main/features/loan/localization/index.js
+++ src/main/features/loan/localization/index.js
@@ -1,9 +1,9 @@
-import en from './en';
-import urdu from './urdu';
+import en from "./en";
+import urdu from "./urdu";
 
-export const loanDictionaryList = {en, urdu}
+export const loanDictionaryList = { en, urdu };
 
 export const languageOptions = {
-    en: 'English',
-    urdu: 'Urdu'
-}
+  en: "English",
+  urdu: "Urdu",
+};

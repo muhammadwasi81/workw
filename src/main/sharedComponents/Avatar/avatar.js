03f24cb12437bc1957438e4493b72c374a24461e
Abu Bakar <danish@miletap.com>
2022-06-10 19:04:50 +0500

    dummy commit

--- src/main/sharedComponents/Avatar/avatar.js
+++ src/main/sharedComponents/Avatar/avatar.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import AvatarGroup from "./AvatarGroup";
 import TagAvatar from "./TagAvatar";
-
+// avatar
 function Avatar(props) {
 	const objProperty =
 		props.heading === "Agents"

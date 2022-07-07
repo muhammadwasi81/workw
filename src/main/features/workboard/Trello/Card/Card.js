b08c92a46000707068c41f6193b2499bba313ce3
Abu Bakar <danish@miletap.com>
2022-07-07 14:21:38 +0500

    added avatar in kanban board of members from details and main card

--- src/main/features/workboard/Trello/Card/Card.js
+++ src/main/features/workboard/Trello/Card/Card.js
@@ -15,6 +15,8 @@ import EditDropDown from "../MenuDropDown/EditDropDown";
 import MemberModal from "../../Modal/MemberModal";
 import EditMembers from "../EditMembers/EditMembers";
 import CardDetailModal from "../Modal/CardDetailModal";
+import { EyeOutlined } from "@ant-design/icons";
+import Avatar from "../../../../sharedComponents/Avatar/avatarOLD";
 
 function Card(props) {
 	const [editing, setEditing] = useState(false);
@@ -87,17 +89,34 @@ function Card(props) {
 							ref={provided.innerRef}
 							{...provided.draggableProps}
 							{...provided.dragHandleProps}
-							className="Card flex items-center relative cusrosr-pointer bg-white m-[5px] p-[5px] rounded-sm border shadow-sm text-sm break-words m-h[18px] group"
+							className="Card items-center relative cusrosr-pointer bg-white m-[5px] p-[5px] rounded-sm border shadow-sm text-sm break-words m-h[18px] group"
 						>
-							<div onClick={openDetail} className="w-full">
-								{cardData.text}
+							<div className="flex mb-3">
+								<div onClick={openDetail} className="w-full">
+									{cardData.text}
+								</div>
+								<EditDropDown
+									className={"edit-icon"}
+									startEditing={startEditing}
+									// deleteList={deleteList}
+									cardId={cardData._id}
+								/>
 							</div>
-							<EditDropDown
-								className={"edit-icon"}
-								startEditing={startEditing}
-								// deleteList={deleteList}
-								cardId={cardData._id}
-							/>
+							{cardData.members && cardData.members.length > 0 && (
+								<div className="flex items-center justify-between">
+									<EyeOutlined />
+									{cardData.members.map(mem => (
+										<Avatar
+											name={mem.name}
+											src={mem.image}
+											round={true}
+											width={"30px"}
+											height={"30px"}
+											isZoom={true}
+										/>
+									))}
+								</div>
+							)}
 						</div>
 					)}
 				</Draggable>

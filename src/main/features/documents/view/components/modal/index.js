e7e306ce32286fd7ae40f2811273c89438c073c6
aqibmemon01 <maqibmemon786@gmail.com>
2022-08-01 20:26:32 +0500

    Test

--- src/main/features/documents/view/components/modal/index.js
+++ src/main/features/documents/view/components/modal/index.js
@@ -1,36 +1,39 @@
 import { Modal } from "antd";
 import React, { useEffect, useState } from "react";
+import { DOCUMENT_ENUM } from "../../../constant";
 
-function PreviewModal({ id, open, setModelState, leftComponent }) {
-  const [visible, setVisible] = useState(false);
+function PreviewModal({ previewItem, handleClose = () => { } }) {
+  // const [visible, setVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [modalText, setModalText] = useState('Content of the modal');
 
   const handleCancel = () => {
-    console.log('Clicked cancel button');
-    setVisible(false);
+    handleClose()
   };
 
   const handleOk = () => {
-    setModalText('The modal will be closed after two seconds');
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setVisible(false);
-      setConfirmLoading(false);
-    }, 2000);
+    handleClose()
   };
 
   return (
     <>
       <Modal
         title="Title"
-        visible={visible}
+        visible={!!previewItem}
         onOk={handleOk}
+        okText="done"
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
       >
         {
-          <img src={path} />
+          previewItem?.documentType === DOCUMENT_ENUM.DUCOMENT_TYPE.image ?
+            <img src={previewItem?.path} /> :
+            (previewItem?.documentType === DOCUMENT_ENUM.DUCOMENT_TYPE.draw || previewItem?.documentType === DOCUMENT_ENUM.DUCOMENT_TYPE.grid ||
+              previewItem?.documentType === DOCUMENT_ENUM.DUCOMENT_TYPE.pad || previewItem?.documentType === DOCUMENT_ENUM.DUCOMENT_TYPE.show)
+              ? <iframe src={previewItem?.path} title="description"
+              width='500px'
+              height='500px' frameBorder='0'></iframe>
+              : ""
         }
       </Modal>
     </>

df1a47adaa16723ddd474214a4052c37c348e51c
Daniyal2108 <70207540+Daniyal2108@users.noreply.github.com>
2022-08-04 14:06:28 +0500

    turkish localization

--- src/main/features/allowance/warning/localization/index.js
+++ src/main/features/allowance/warning/localization/index.js
@@ -1,11 +1,13 @@
 import en from "./en";
 import urdu from "./urdu";
 import hindi from "./hindi";
+import turkish from "./turkish";
 
-export const warningDictionaryList = { en, urdu, hindi };
+export const warningDictionaryList = { en, urdu, hindi, turkish };
 
 export const languageOptions = {
   en: "English",
   urdu: "Urdu",
   hindi: "Hindi",
+  turkish: "Turkish",
 };
---
+++ src/main/features/feed/localization/turkish.js
@@ -0,0 +1,25 @@
+const turkish = {
+  Direction: "ltr",
+
+  composer: {
+    Whatsonyourmind: "Aklınızdan ne geçiyor",
+    Write: "Yazın",
+    WriteYourName: "Adınızı Yazın",
+    Poll: "Anket",
+    Documents: "Belgeler",
+    PhotoVideo: "Fotoğraf/Video",
+    Post: "Yayınla",
+    Removepoll: "Anketi kaldır",
+    Addpolloption: "Anket seçeneği ekle",
+  },
+  Post: {
+    Comments: "Yorumlar",
+    Comment: "Yazın0",
+    Share: "Yazın1",
+    Like: "Yazın2",
+    ViewAllComments: "Yazın3",
+    WriteYourCommentHere: "Yazın4",
+    WriteYourReplyHere: "Yazın5",
+  },
+};
+export default turkish;

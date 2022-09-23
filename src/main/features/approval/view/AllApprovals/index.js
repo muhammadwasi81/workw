a4f1cdeb462a939e0ee6c3520e8f5aad9b8eee76
Abu Bakar <danish@miletap.com>
2022-09-23 15:26:05 +0500

    approval work

--- src/main/features/approval/view/AllApprovals/index.js
+++ src/main/features/approval/view/AllApprovals/index.js
@@ -9,54 +9,61 @@ import ApprovalDetail from "./detail";
 import Listing from "./listing";
 
 export default function AllApprovals() {
-  const [tableView, setTableView] = useState(false);
+	const [tableView, setTableView] = useState(false);
 
-  return (
-    <TabbableContainer>
-        <Header
-          buttons={[
-            {
-              buttonText: "Create Travel",
-              // onClick: () => setVisible(true),
-              render: (
-                <SideDrawer title={"Hello"} buttonText={"Hello"} isAccessDrawer={false}>
-                  "Hello"
-                </SideDrawer>
-              ),
-            },
-          ]}
-        />
-        <TopBar
-          onSearch={(value) => {
-            console.log(value);
-          }}
-          buttons={[
-            {
-              name: "Filter",
-            }
-          ]}
-          segment={{
-            onSegment: (value) => {
-              if (value === "Table") {
-                setTableView(true);
-              } else {
-                setTableView(false);
-              }
-            },
-            label1: "List",
-            label2: "Table",
-          }}
-        />
-        <ContBody>
-        <div className="flex ApprovalMainView gap-4 w-full">
-          <div className="">
-            <Listing />
-          </div>
-          <div className="flex-1" >
-            <ApprovalDetail />
-          </div>
-        </div>
-        </ContBody>
-    </TabbableContainer>
-  )
+	return (
+		<TabbableContainer>
+			<Header
+				buttons={
+					[
+						// {
+						// 	buttonText: "Create Travel",
+						// 	// onClick: () => setVisible(true),
+						// 	render: (
+						// 		<SideDrawer
+						// 			title={"Hello"}
+						// 			buttonText={"Hello"}
+						// 			isAccessDrawer={false}
+						// 		>
+						// 			"Hello"
+						// 		</SideDrawer>
+						// 	),
+						// },
+					]
+				}
+				backButton={false}
+			/>
+			<TopBar
+				onSearch={value => {
+					console.log(value);
+				}}
+				buttons={[
+					{
+						name: "Filter",
+					},
+				]}
+				segment={{
+					onSegment: value => {
+						if (value === "Table") {
+							setTableView(true);
+						} else {
+							setTableView(false);
+						}
+					},
+					label1: "List",
+					label2: "Table",
+				}}
+			/>
+			<ContBody>
+				<div className="flex ApprovalMainView gap-4 w-full">
+					<div className="">
+						<Listing />
+					</div>
+					<div className="flex-1">
+						<ApprovalDetail />
+					</div>
+				</div>
+			</ContBody>
+		</TabbableContainer>
+	);
 }

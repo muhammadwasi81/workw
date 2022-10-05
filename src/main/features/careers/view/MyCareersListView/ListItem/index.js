65bfc4be20d1ecb5bba83c61b3850036cfda5c47
aqibmemon01 <maqibmemon786@gmail.com>
2022-10-05 17:07:57 +0500

    working on careers

--- src/main/features/careers/view/MyCareersListView/ListItem/index.js
+++ src/main/features/careers/view/MyCareersListView/ListItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, Image } from "antd"; 
+import { Button, Image } from "antd";
 import UserInfo from "../../../../../sharedComponents/UserShortInfo/UserInfo";
 import SublineDesigWithTime from "../../../../../sharedComponents/UserShortInfo/SubLine/DesigWithTime";
 import { getNameForImage } from "../../../../../../utils/base";
@@ -16,13 +16,30 @@ function ListItem(props) {
         <ItemHeader className="ItemHeader">
           <div className="flex items-center gap-3">
             <Avatar src={undefined} className="addPostAvatar" name={"Test Test"} width={40} height={40} round={true} />
-            <div className="font-bold text-[15px]" >Miletap</div>
+            <div className="font-bold text-[15px] text-primary-color" >Miletap</div>
           </div>
         </ItemHeader>
-        <ItemContent className="ItemContent">
-          <h3> React Js Developer </h3>
-          <p>  Karachi, Pakistan - Full Time </p>
+        <ItemContent className="!h-[100px] !max-h-[100px]" >
+          <div className="font-bold text-[14px] text-primary-color"> React Js Developer </div >
+          <p className="careersDescShort" >
+            Karachi,
+            Pakistan - Full Time Karachi,
+            Pakistan - Full Time Karachi,
+            Pakistan - Full Time Karachi,
+            Pakistan - Full Time Karachi,
+            Pakistan - Full Time  Karachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full Time  Karachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi,
+            Pakistan - Full TimeKarachi, Pakistan - Full TimeKarachi, Pakistan - Full TimeKarachi, Pakistan - Full Time
+            Karachi, Pakistan - Full TimeKarachi, Pakistan - Full TimeKarachi, Pakistan - Full TimeKarachi, Pakistan - Full Time </p>
         </ItemContent>
+        
       </SingleItem>
     </>
   );

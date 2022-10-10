3b4de632ae9afa8e30389e831d777d3fe6a52663
Abu Bakar <danish@miletap.com>
2022-10-10 16:59:21 +0500

    feed work

--- src/main/features/feed/ui/posts_list/index.js
+++ src/main/features/feed/ui/posts_list/index.js
@@ -1,17 +1,20 @@
 import PostItem from "./post/index";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllFeed } from "../../store/actions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PostSkeleton from "./post/skeleton/post";
 import { ReactionModuleEnum } from "../../../../../utils/Shared/enums/enums";
+import Scroll from "../../../../sharedComponents/ScrollSelect/infinteScoll";
 
 function PostsList({ referenceType, referenceId, reactionModule }) {
 	const { userSlice, feedSlice } = useSelector(state => state);
+	const [pageNo, setPageNo] = useState(1);
+	const [feed, setFeed] = useState([]);
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(
 			getAllFeed({
-				pageNo: 1,
+				pageNo,
 				pageSize: 20,
 				search: "",
 				referenceId: referenceId ? referenceId : userSlice.user.id,
@@ -19,25 +22,38 @@ function PostsList({ referenceType, referenceId, reactionModule }) {
 				filterType: 1,
 			})
 		);
-	}, []);
+	}, [pageNo]);
+	const { posts } = feedSlice.allFeed;
 
-	if (feedSlice.allFeed.loading) return <PostSkeleton />;
+	useEffect(() => {
+		setFeed([...feed, ...posts]);
+	}, [JSON.stringify(posts)]);
+
+	// if (feedSlice.allFeed.loading) return <PostSkeleton />;
 	return (
-		<div className="newsList ">
-			{!feedSlice.allFeed.posts.length > 0 ? (
-				<p>No Posts</p>
-			) : (
-				feedSlice.allFeed.posts.map(post => (
-					<PostItem
-						post={post}
-						viewAllComments={true}
-						referenceType={referenceType}
-						referenceId={referenceId}
-						reactionModule={reactionModule}
-					/>
-				))
-			)}
-		</div>
+		<Scroll
+			data={feed}
+			fetchMoreData={pageNo => {
+				setPageNo(pageNo);
+			}}
+			height={981}
+		>
+			<div className="newsList ">
+				{!feed.length > 0 ? (
+					<p>No Posts</p>
+				) : (
+					feed.map(post => (
+						<PostItem
+							post={post}
+							viewAllComments={true}
+							referenceType={referenceType}
+							referenceId={referenceId}
+							reactionModule={reactionModule}
+						/>
+					))
+				)}
+			</div>
+		</Scroll>
 	);
 }
 
--- src/main/sharedComponents/ScrollSelect/infinteScoll.js
+++ src/main/sharedComponents/ScrollSelect/infinteScoll.js
@@ -2,42 +2,78 @@ import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import "./style.css";
 const style = {
-	height: 30,
+	// height: 30,
 	border: "1px solid green",
-	margin: 6,
-	padding: 8,
+	// margin: 6,
+	// padding: 8,
 };
 
 export default class Scroll extends React.Component {
 	state = {
-		items: Array.from({ length: 20 }),
+		page: 1,
+		height: "100%",
+		// items: Array.from({ length: 20 }),
 	};
+
+	componentDidMount() {
+		//set height
+		console.log(
+			"dsfdsf",
+			document.getElementsByClassName("scroll_dropdown")[0].offsetParent
+				.offsetHeight
+		);
+		this.setState({
+			height: document.getElementsByClassName("scroll_dropdown")[0]
+				.offsetParent.offsetHeight,
+		});
+	}
 
 	fetchMoreData = () => {
 		// a fake async api call like which sends
 		// 20 more records in 1.5 secs
-		setTimeout(() => {
-			this.setState({
-				items: this.state.items.concat(Array.from({ length: 20 })),
-			});
-		}, 1500);
+		// setTimeout(() => {
+		// 	this.setState({
+		// 		items: this.state.items.concat(Array.from({ length: 20 })),
+		// 	});
+		// }, 1500);
+
+		//increase the number of page by 1
+		// console.log("fetch more data");
+		this.setState({
+			page: this.state.page + 1,
+		});
 	};
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.page !== this.state.page) {
+			// console.log("callback");
+			this.props.fetchMoreData(this.state.page);
+		}
+		// if (
+		// 	JSON.stringify(prevProps.data) !==
+		// 		JSON.stringify(this.props.data) &&
+		// 	this.props.data?.length > 0
+		// ) {
+		// 	this.fetchMoreData();
+		// }
+	}
 
 	render() {
 		return (
 			<div className="scroll_dropdown">
 				<InfiniteScroll
-					dataLength={this.state.items.length}
+					dataLength={this.props.data.length}
 					next={this.fetchMoreData}
 					hasMore={true}
 					loader={<h4>Loading...</h4>}
-					height={250}
+					height={this.state.height}
+					endMessage={
+						<p style={{ textAlign: "center" }}>
+							<b>Yay! You have seen it all</b>
+						</p>
+					}
 				>
-					{this.state.items.map((i, index) => (
-						<div style={style} key={index}>
-							div - #{index}
-						</div>
-					))}
+					{this.props.children}
 				</InfiniteScroll>
 			</div>
 		);

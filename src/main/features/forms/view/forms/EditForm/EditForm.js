05028cea4a62f9bb786142d2a4fae1e6b3be8040
Humayoun shah <humayun@miletap.com>
2022-10-10 13:03:32 +0500

    add question in edit section

--- src/main/features/forms/view/forms/EditForm/EditForm.js
+++ src/main/features/forms/view/forms/EditForm/EditForm.js
@@ -63,7 +63,7 @@ const EditForm = (props) => {
   const [firstTimeEmpData, setFirstTimeEmpData] = useState([]);
   const [employeesData, setEmployeesData] = useState([]);
   const [isFirstTimeDataLoaded, setIsFirstTimeDataLoaded] = useState(false);
-  const [dataObj, setDataObj] = useState(initialData);
+  const [dataObj, setDataObj] = useState([]);
   const [formData, setFormData] = useState(null);
   const [question, setQuestions] = useState([]);
 
@@ -101,70 +101,17 @@ const EditForm = (props) => {
     console.log("useEffect works when component update");
     // console.log("***", formDetail);
     if (Object.keys(formDetail).length > 1) {
-      setFormDataByType(formDetail);
+      setDataObj(formDetail);
+      // setFormDataByType(formDetail);
     }
   }, [formDetail]);
 
-  // useEffect(() => {
-  //   const append = (answers, fileList) =>
-  //     answers.map((x, index) => {
-  //       let image = fileList.filter((it) => it.index === index)[0]?.image
-  //         ?.originFileObj;
-  //       return {
-  //         answer: x,
-  //         image: {
-  //           file: image,
-  //           id: defaultUiid,
-  //         },
-  //       };
-  //     });
-  //   let questionArray = question.map((elem, index) => {
-  //     console.log("element", elem);
-  //     return {
-  //       // id: createGuid(),
-  //       // formId: createGuid(),
-  //       answerType: elem.answerType,
-  //       sequence: index,
-  //       question: elem.Question,
-  //       createBy: user.id,
-  //       answers: elem.options
-  //         ? // ? append(elem.options, elem.fileList[index]?.originFileObj)
-  //           append(elem.options, elem.fileList)
-  //         : [],
-  //     };
-  //   });
-  //   // console.log("****", questionArray);
-  //   setDataObj({ ...dataObj, question: questionArray });
-  // }, [question]);
-
-  // const dataGet = (values) => {
-  //   console.log("data getting from create form component", values);
-  //   setQuestions([...question, values]);
-  // };
-
-  // const createForm = () => {
-  //   // console.log("create form done!!!!");
-  //   // console.log("data object", dataObj);
-  //   dispatch(addForm(dataObj));
-  // };
-
-  // const subDescriptionGet = (values) => {
-  //   // console.log("sub description", values);
-  //   let payload = {
-  //     ...dataObj,
-  //     id: createGuid(),
-  //     name: values.subject,
-  //     description: values.description,
-  //     approvers: modifySelectData(values.approvers).map((el, index) => {
-  //       return {
-  //         approverId: el,
-  //       };
-  //     }),
-  //   };
-  //   setDataObj(payload);
-  //   // console.log("final data to be send to api****", payload);
-  //   dispatch(addForm(payload));
-  // };
+  useEffect(() => {
+    console.log("use effect works when data object change****", dataObj);
+    if (Object.keys(dataObj).length > 1) {
+      setFormDataByType(dataObj);
+    }
+  }, [dataObj]);
 
   let setFormDataByType = (data) => {
     console.log("data getting in set form by type****", data);
@@ -329,8 +276,41 @@ const EditForm = (props) => {
   };
 
   const dataGet = (values) => {
-    console.log("data get in create form", values);
-    //TODO: modify this data like questions and append it with questions data
+    const append = (answers, fileList) =>
+      answers.map((x, index) => {
+        let image =
+          fileList &&
+          fileList?.filter((it) => it.index === index)[0]?.image?.originFileObj;
+        return {
+          answer: x,
+          image: {
+            file: image && image,
+            id: defaultUiid,
+          },
+        };
+      });
+
+    let ques = {
+      formAnswerType: values.formAnswerType,
+      // sequence: index,
+      question: values.question,
+      image: {
+        file:
+          values.image &&
+          (values.image?.originFileObj
+            ? values.image?.originFileObj
+            : values.image.file),
+      },
+      createBy: user.id,
+      answers: values.fileList ? append(values.answers, values?.fileList) : [],
+    };
+    console.log(ques, "new question");
+
+    //TODO: append question in dataObj
+    let data = [...dataObj.question];
+    console.log(data, "data spread");
+    data.push(ques);
+    setDataObj({ ...dataObj, question: data });
   };
 
   const onEdit = () => {

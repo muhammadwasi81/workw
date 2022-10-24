e46833e8bdbbe219190ef551d93f0bdfeeca4b58
Syed Danish Ali <danish@miletap.com>
2022-10-24 10:18:05 +0500

    fixed conflicts

--- src/main/features/requisition/view/myRequisitionDetail/tableColumn.js
+++ src/main/features/requisition/view/myRequisitionDetail/tableColumn.js
@@ -1,53 +1,50 @@
 import StatusTag from "../../../../sharedComponents/Tag/StatusTag";
 import { requisitionDictionaryList } from "../../localization/index";
-import { LanguageChangeContext } from "../../../../../utils/localization/localContext/LocalContext";
-import { useContext } from "react";
 
-export const tableColumn = () => {
-  const { userLanguage } = useContext(LanguageChangeContext);
+export const tableColumn = userLanguage => {
 	const { requisitionDictionary } = requisitionDictionaryList[userLanguage];
-  return [
-    {
-      title: requisitionDictionary.name,
-      dataIndex: "name",
-      ellipsis: true,
-      sort: true,
-    },
-    {
-      title: requisitionDictionary.PhoneNumber,
-      dataIndex: "phoneNumber",
-      ellipsis: true,
-      sort: true,
-    },
-    {
-      title: requisitionDictionary.Email,
-      dataIndex: "email",
-      ellipsis: true,
-      sort: true,
-    },
-    {
-      title: requisitionDictionary.Offer,
-      dataIndex: "offer",
-      ellipsis: true,
-      sort: true,
-    },
-    {
-      title: requisitionDictionary.BusinessName,
-      dataIndex: "businessName",
-      ellipsis: true,
-      sort: true,
-    },
-    {
-        title: requisitionDictionary.BusinessAddress,
-        dataIndex: "businessAddress",
-        ellipsis: true,
-        sort: true,
-      },
-    {
-      title: requisitionDictionary.Status,
-      dataIndex: "status",
-      ellipsis: true,
-      render: (status) => <StatusTag status={status} />,
-    },
-  ];
+	return [
+		{
+			title: requisitionDictionary.name,
+			dataIndex: "name",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.PhoneNumber,
+			dataIndex: "phoneNumber",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.Email,
+			dataIndex: "email",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.Offer,
+			dataIndex: "offer",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.BusinessName,
+			dataIndex: "businessName",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.BusinessAddress,
+			dataIndex: "businessAddress",
+			ellipsis: true,
+			sort: true,
+		},
+		{
+			title: requisitionDictionary.Status,
+			dataIndex: "status",
+			ellipsis: true,
+			render: status => <StatusTag status={status} />,
+		},
+	];
 };

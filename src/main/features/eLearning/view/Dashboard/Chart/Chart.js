3f8bf0be73e7f2112340ee28b0d2a1ad369a54ce
muhammadwasi81 <wasiarain819@gmail.com>
2022-10-27 17:51:49 +0500

    format: code

--- src/main/features/eLearning/view/Dashboard/Chart/Chart.js
+++ src/main/features/eLearning/view/Dashboard/Chart/Chart.js
@@ -32,18 +32,19 @@ function Chart() {
     'July',
   ];
 
-<<<<<<< HEAD
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'left',
+        position: 'top',
         display: false,
       },
       title: {
         display: true,
         text: 'Activity',
-        position: 'left',
+        position: 'top',
+        align: 'start',
+        color: '#526bb1',
       },
     },
   };
@@ -69,46 +70,6 @@ function Chart() {
       <Line options={options} data={data} height={100} />
     </div>
   );
-=======
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "top",
-				display: false,
-			},
-			title: {
-				display: true,
-				text: "Activity",
-				position: "top",
-				align: "start",
-				color: "#526bb1",
-			},
-		},
-	};
-	const data = {
-		labels,
-		datasets: [
-			{
-				label: "This week",
-				data: [33, 53, 85, 41, 44, 65, 1],
-				borderColor: "#526bb1",
-				pointBackgroundColor: "#fff",
-				pointBorderColor: "#526bb1",
-				pointHoverBackgroundColor: "#526bb1",
-				pointHoverBorderColor: "#fff",
-				pointRadius: 5,
-				pointHoverRadius: 5,
-				fill: true,
-			},
-		],
-	};
-	return (
-		<div>
-			<Line options={options} data={data} height={100} />
-		</div>
-	);
->>>>>>> 5025d3abfff23bd2dc4573158e05e33d856aaf3e
 }
 
 export default Chart;
--- src/main/features/schedule/index.js
+++ src/main/features/schedule/index.js
@@ -1,5 +1,4 @@
 import {
-<<<<<<< HEAD
   ContBody,
   TabbableContainer,
 } from '../../sharedComponents/AppComponents/MainFlexContainer';
@@ -8,20 +7,15 @@ import Header from './components/header';
 import Calendar from './view/calendar';
 import { useSearchParams } from 'react-router-dom';
 import MySchedules from './view/schedules/Schedules';
-// import ScheduleTopBar from './view/Schedules/topbar/ScheduleTopBar';
+import { ScheduleTopBar } from './view/schedules/topbar/ScheduleTopBar';
 
 function Schedules() {
-  // const [param, setParam] = useState("");
   const [searchParams] = useSearchParams();
-  // useEffect(() => {
-  // 	setParam(searchParams.get("f"));
-  // }, [searchParams]);
   let param = searchParams.get('f');
-  // console.log("param", param);
   return (
     <TabbableContainer>
       <Header />
-      {/* <ScheduleTopBar /> */}
+      <ScheduleTopBar />
       <ContBody style={{ display: 'block' }}>
         {param === 'cal' ? (
           <Calendar />
@@ -33,36 +27,6 @@ function Schedules() {
       </ContBody>
     </TabbableContainer>
   );
-=======
-	ContBody,
-	TabbableContainer,
-} from "../../sharedComponents/AppComponents/MainFlexContainer";
-import "./styles/style.css";
-import Header from "./components/header";
-import Calendar from "./view/calendar";
-import { useSearchParams } from "react-router-dom";
-import MySchedules from "./view/schedules/Schedules";
-import { ScheduleTopBar } from "./view/schedules/topbar/ScheduleTopBar";
-
-function Schedules() {
-	const [searchParams] = useSearchParams();
-	let param = searchParams.get("f");
-	return (
-		<TabbableContainer>
-			<Header />
-			<ScheduleTopBar />
-			<ContBody style={{ display: "block" }}>
-				{param === "cal" ? (
-					<Calendar />
-				) : param === "sc" ? (
-					<MySchedules />
-				) : (
-					<>div</>
-				)}
-			</ContBody>
-		</TabbableContainer>
-	);
->>>>>>> 5025d3abfff23bd2dc4573158e05e33d856aaf3e
 }
 
 export default Schedules;

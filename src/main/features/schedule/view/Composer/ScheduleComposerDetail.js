41c1c66f67560c9dc31e073484eda7a563f500f7
Syed Danish Ali <danish@miletap.com>
2022-12-12 19:02:09 +0500

    schedule work

--- src/main/features/schedule/view/Composer/ScheduleComposerDetail.js
+++ src/main/features/schedule/view/Composer/ScheduleComposerDetail.js
@@ -11,11 +11,12 @@ import Event from "../event";
 
 import ScheduleMembersList from "../Composer/ScheduleMembersList";
 import ScheduleDetailSkeleton from "./ScheduleDetailSkeleton";
-import { EditOutlined } from "@ant-design/icons";
-import { Button, Tooltip } from "antd";
-import CreateSchedule from "../createSchedule";
+// import { EditOutlined } from "@ant-design/icons";
+// import CreateSchedule from "../createSchedule";
+import { Button } from "antd";
 import UpdateSchedule from "./UpdateSchedule";
 import Attachments from "../../../travel/view/UI/Attachments";
+import CommentWrapper from "../../../../sharedComponents/Comment/CommentWrapper";
 
 function ScheduleComposerDetail({ id, shortEvent = true }) {
 	const eventDetail = useSelector(state => state.scheduleSlice.eventDetail);
@@ -70,18 +71,19 @@ function ScheduleComposerDetail({ id, shortEvent = true }) {
 			{loading ? (
 				<ScheduleDetailSkeleton />
 			) : (
-				<div className={`eventDetail ${!shortEvent && ""}`}>
-					{shortEvent && (
-						<div className="eventDetail__header">
-							<p className="eventDetail-title">Details</p>
-							{/* <span className="eventNum">SCH-000085</span> */}
-							<Button
-								className="ThemeBtn"
-								onClick={handleEditSchedule}
-							>
-								Update
-							</Button>
-							{/* <Tooltip title="Edit Schedule">
+				<>
+					<div className={`eventDetail ${!shortEvent && ""}`}>
+						{shortEvent && (
+							<div className="eventDetail__header">
+								<p className="eventDetail-title">Details</p>
+								{/* <span className="eventNum">SCH-000085</span> */}
+								<Button
+									className="ThemeBtn"
+									onClick={handleEditSchedule}
+								>
+									Update
+								</Button>
+								{/* <Tooltip title="Edit Schedule">
 										<EditOutlined
 											className="!text-primary-color cursor-pointer"
 											onClick={() => {
@@ -89,57 +91,73 @@ function ScheduleComposerDetail({ id, shortEvent = true }) {
 											}}
 										/>
 									</Tooltip> */}
-						</div>
-					)}
-					<div className="eventDetail__body">
-						<div className="eventDetail__body-event">
-							{shortEvent ? (
-								<Event shortDesc={true} data={eventDetail} />
-							) : (
-								<EventDetail data={eventDetail} />
-							)}
-						</div>
-						{!shortEvent && (
-							<div className="eventDetail__body-description">
-								<p className="eventDetail-title">Description</p>
-								<div
-									dangerouslySetInnerHTML={{
-										__html: eventDetail?.description,
-									}}
-									className="break-words dangerous_element_detail"
-								/>
 							</div>
 						)}
-					</div>
-					{eventDetail?.attachments?.length > 0 ? (
-						<>
-							<div className="eventDetail-title">Attachments</div>
-							<div className="!w-max m-4">
-								<Attachments
-									data={eventDetail?.attachments}
-									key={{ data: eventDetail?.attachments }}
-									toShow={1}
-									onClick={() => {}}
-									size={"50px"}
-								/>
+						<div className="eventDetail__body">
+							<div className="eventDetail__body-event">
+								{shortEvent ? (
+									<Event
+										shortDesc={true}
+										data={eventDetail}
+									/>
+								) : (
+									<EventDetail data={eventDetail} />
+								)}
 							</div>
-						</>
-					) : null}
-					<div className="eventDetail-title">Members</div>
-					{eventDetail?.members?.map(member => (
-						<ScheduleMembersList
-							status={member.status}
-							id={member.id}
-							data={member?.member}
-							memberType={member.memberType}
-							isActionEnabled={isActionEnabled}
-							handleMemberStatusChange={handleMemberStatusChange}
-							handleMemberTypeStatusChange={
-								handleMemberTypeStatusChange
-							}
-						/>
-					))}
-				</div>
+							{!shortEvent && (
+								<div className="eventDetail__body-description">
+									<p className="eventDetail-title">
+										Description
+									</p>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: eventDetail?.description,
+										}}
+										className="break-words dangerous_element_detail"
+									/>
+								</div>
+							)}
+						</div>
+						{eventDetail?.attachments?.length > 0 ? (
+							<>
+								<div className="eventDetail-title">
+									Attachments
+								</div>
+								<div className="!w-max m-4">
+									<Attachments
+										data={eventDetail?.attachments}
+										key={{ data: eventDetail?.attachments }}
+										toShow={1}
+										onClick={() => {}}
+										size={"50px"}
+									/>
+								</div>
+							</>
+						) : null}
+						<div className="eventDetail-title">Members</div>
+						{eventDetail?.members?.map(member => (
+							<ScheduleMembersList
+								status={member.status}
+								id={member.id}
+								data={member?.member}
+								memberType={member.memberType}
+								isActionEnabled={isActionEnabled}
+								handleMemberStatusChange={
+									handleMemberStatusChange
+								}
+								handleMemberTypeStatusChange={
+									handleMemberTypeStatusChange
+								}
+							/>
+						))}
+					</div>
+					<CommentWrapper
+						referenceId={eventDetail?.id}
+						// isCommentLoad={true}
+						module={12}
+						// loadSkeleton={true}
+					/>
+				</>
 			)}
 			<UpdateSchedule
 				eventDetail={eventDetail}

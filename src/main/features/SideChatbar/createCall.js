0e5f7ee446f92e71bdc382d764c5be754582af24
Humayoun shah <humayun@miletap.com>
2022-12-24 16:28:58 +0500

    icons

--- src/main/features/SideChatbar/createCall.js
+++ src/main/features/SideChatbar/createCall.js
@@ -8,67 +8,78 @@ import CreateRoom from "../calling/components/createRoom/CreateRoom";
 import { sideChatBarList } from "./localization";
 import { handleCreateRoomModal } from "../calling/store/slice";
 import { instantCall } from "../calling/store/action";
+import roomMeeting from "../../../content/NewContent/Sidebar/svg/roomMeeting.svg";
+import call from "../../../content/NewContent/Sidebar/svg/call.svg";
+
 export const CreateCall = () => {
-	const dispatch = useDispatch();
-	const isCreateRoomModalOpen = useSelector(
-		state => state.callingSlice.isCreateRoomModalOpen
-	);
-	const { userLanguage } = useContext(LanguageChangeContext);
-	const { createRoom, instantCall: icall } = sideChatBarList[userLanguage];
-	// const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
+  const isCreateRoomModalOpen = useSelector(
+    (state) => state.callingSlice.isCreateRoomModalOpen
+  );
+  const { userLanguage } = useContext(LanguageChangeContext);
+  const { createRoom, instantCall: icall } = sideChatBarList[userLanguage];
+  // const [visible, setVisible] = useState(false);
 
-	const menu = (
-		<Menu
-			selectable
-			items={[
-				{
-					key: "1",
-					label: createRoom,
-					onClick: () => {
-						console.log("create room cliced");
-						dispatch(handleCreateRoomModal(true));
-					},
-				},
-				{
-					key: "2",
-					label: icall,
-					onClick: () => {
-						dispatch(
-							instantCall({ isPrivate: false, roomPassword: "" })
-						);
-					},
-				},
-			]}
-		/>
-	);
+  const menu = (
+    <Menu
+      selectable
+      items={[
+        {
+          key: "1",
+          label: (
+            <div className="flex">
+              <img src={roomMeeting} alt="" style={{ height: "1.3rem" }} />
+              {createRoom}
+            </div>
+          ),
+          onClick: () => {
+            console.log("create room cliced");
+            dispatch(handleCreateRoomModal(true));
+          },
+        },
+        {
+          key: "2",
+          label: (
+            <div className="flex">
+              <img src={call} alt="" style={{ height: "1.3rem" }} />
+              {icall}
+            </div>
+          ),
+          onClick: () => {
+            dispatch(instantCall({ isPrivate: false, roomPassword: "" }));
+          },
+        },
+      ]}
+    />
+  );
 
-	return (
-		<>
-			<div>
-				<Dropdown overlay={menu}>
-					<Typography.Link>
-						<Space>
-							<VideoCameraOutlined />
-						</Space>
-					</Typography.Link>
-				</Dropdown>
-			</div>
+  return (
+    <>
+      <div>
+        <Dropdown overlay={menu}>
+          <Typography.Link>
+            <Space>
+              <VideoCameraOutlined />
+            </Space>
+          </Typography.Link>
+        </Dropdown>
+      </div>
 
-			<Modal
-				title=""
-				centered
-				visible={isCreateRoomModalOpen}
-				onCancel={() => {
-					dispatch(handleCreateRoomModal(false));
-				}}
-				width={1000}
-				footer={null}
-				destroyOnClose={true}
-			>
-				<CreateRoom />
-			</Modal>
-		</>
-	);
+      <Modal
+        title=""
+        centered
+        visible={isCreateRoomModalOpen}
+        onCancel={() => {
+          dispatch(handleCreateRoomModal(false));
+        }}
+        width={1000}
+        footer={null}
+        destroyOnClose={true}
+      >
+        <CreateRoom />
+      </Modal>
+    </>
+  );
 };
 
 export default CreateCall;

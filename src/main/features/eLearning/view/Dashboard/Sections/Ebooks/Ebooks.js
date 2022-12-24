07e044decd2843187a2a0d2b9503bbc0445a9d41
salman <salman@miletap.com>
2022-12-24 14:02:34 +0500

    Add Document Directory Modal Done

--- src/main/features/eLearning/view/Dashboard/Sections/Ebooks/Ebooks.js
+++ src/main/features/eLearning/view/Dashboard/Sections/Ebooks/Ebooks.js
@@ -1,17 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllBook } from "../../../../store/action";
 import EbookCard from "../../Components/EbookCard";
+import { NoDataFound } from "../../../../../../sharedComponents/NoDataIcon";
 
-function Ebooks() {
+function Books() {
+	const dispatch = useDispatch()
+	const [filter, setFilter] = useState({ pageNo: 1, pageSize: 20, search: "", sortBy: 1, filterType: 1 })
+
+	const { books } = useSelector((state) => state.eLearningSlice);
+
+	console.log(books, "BOOKS")
+
+	useEffect(() => {
+		dispatch(getAllBook(filter))
+	}, [])
+
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mb-2">
-			<EbookCard />
-			<EbookCard />
-			<EbookCard />
-			<EbookCard />
-			<EbookCard />
-			<EbookCard />
-		</div>
+		<>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mb-2">
+				{
+					books?.length > 0 ? books.map((item) => {
+						return <EbookCard data={item} />
+					}) : ""
+				}
+			</div>
+			<div className="flex justify-center">
+			{books?.length === 0 ? <NoDataFound /> : ""} 
+			</div>
+		</>
 	);
 }
 
-export default Ebooks;
+export default Books;
+

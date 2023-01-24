e991e1c715b5a7efc227b08fda6c71aff5e23483
Humayoun shah <humayun@miletap.com>
2023-01-24 14:58:57 +0500

    emergency info

--- src/main/features/emergencyInfo/service/service.js
+++ src/main/features/emergencyInfo/service/service.js
@@ -1,9 +1,9 @@
 import {
   ResponseResultError,
   ResponseResultSuccess,
-} from '../../../../utils/api/ResponseResult';
-import Config from '../../../../utils/services/MasterConfig';
-import MasterConfig from '../../../../utils/services/MasterConfig';
+} from "../../../../utils/api/ResponseResult";
+import Config from "../../../../utils/services/MasterConfig";
+import MasterConfig from "../../../../utils/services/MasterConfig";
 
 const API_PREFIX = `api/`;
 
@@ -27,7 +27,28 @@ export const updateUserEmployeeContactService = async (payload) => {
       `${API_PREFIX}userEmergencyContact/UpdateUserEmergencyContact`,
       payload
     );
-    console.log(data, 'updateUserEmployeeContactService service');
+    console.log(data, "updateUserEmployeeContactService service");
+    if (responseCode === 1001) return ResponseResultSuccess(data);
+    return ResponseResultError(message);
+  } catch (e) {
+    return ResponseResultError(e);
+  }
+};
+
+export const addUserEmergenctContactService = async (payload) => {
+  console.log(payload, "payload", payload.id);
+  const newPayload = {
+    ...payload.payload,
+    userId: payload.id,
+  };
+  try {
+    const {
+      data: { responseCode, data, message },
+    } = await Config.post(
+      `${API_PREFIX}UserEmergencyContact/AddUserEmergencyContact?userId=${payload.id}`,
+      newPayload
+    );
+    console.log(data, "AddUserEmergencyContact service");
     if (responseCode === 1001) return ResponseResultSuccess(data);
     return ResponseResultError(message);
   } catch (e) {

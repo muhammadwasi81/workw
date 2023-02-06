627fcd7cc00d3d2e50556c5ae386dafc471209e3
Hajra <hajrabuksh@gmail.com>
2023-02-06 12:04:07 -0800

    activityLog layout set

--- src/main/features/resignation/store/slice.js
+++ src/main/features/resignation/store/slice.js
@@ -49,6 +49,7 @@ const resignationSlice = createSlice({
         state.success = true
         state.createLoader = false
         state.items = [payload.data.data, ...state.items];
+        console.log("sliceresignationn",payload);
       })
       .addMatcher(isPending(...[getAllResignations]), (state) => {
         state.loader = true;

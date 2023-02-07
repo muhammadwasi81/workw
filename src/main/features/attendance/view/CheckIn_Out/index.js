e5a03f40cbcb4ecf574a8682d22c7b2a6d9c3dd0
Sanjana Bajaj <sanjana@miletap.com>
2023-02-07 12:32:08 +0500

    feed header day format on small screen

--- src/main/features/attendance/view/CheckIn_Out/index.js
+++ src/main/features/attendance/view/CheckIn_Out/index.js
@@ -12,62 +12,64 @@ import { useDispatch } from "react-redux";
 import { getAttendanceLastCheckIn } from "../../store/actions";
 import { ATTENDANCE_ENUM } from "../../utils/constant";
 import CheckInList from "./checkInList";
+import { useMediaQuery } from "react-responsive";
 
 const CheckIn = () => {
-	const dispatch = useDispatch();
-	const { CHECK_IN } = ATTENDANCE_ENUM;
-	const [isOpen, setIsOpen] = useState(false);
-	const { lastCheckIn, success } = useSelector(
-		state => state.attendanceSlice
-	);
+  const dispatch = useDispatch();
+  const { CHECK_IN } = ATTENDANCE_ENUM;
+  const [isOpen, setIsOpen] = useState(false);
+  const { lastCheckIn, success } = useSelector(
+    (state) => state.attendanceSlice
+  );
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
 
-	useEffect(() => {
-		dispatch(getAttendanceLastCheckIn());
-	}, [isOpen]);
-	useEffect(() => {
-		if (success) setIsOpen(false);
-	}, [success]);
-	return (
-		<>
-			<div className="flex items-center text-xs">
-				<div>{moment().format("LLLL")}</div>
-				<div className="cursor-pointer" onClick={() => setIsOpen(true)}>
-					<img
-						className={
-							"checkInBtn ml-2 " +
-							(lastCheckIn && lastCheckIn.type === CHECK_IN.IN
-								? "blinkInfoRed"
-								: "blinkInfo")
-						}
-						src={
-							lastCheckIn && lastCheckIn.type === CHECK_IN.IN
-								? ClockOut
-								: ClockIn
-						}
-						alt=""
-					/>
-				</div>
-			</div>
-			<Modal
-				title={null}
-				centered
-				visible={isOpen}
-				footer={null}
-				closeIcon={null}
-				className="checkInModal"
-				onOk={() => setIsOpen(false)}
-				onCancel={() => setIsOpen(false)}
-				width="445px"
-			>
-				<CheckInHeader />
-				<CheckInCard
-					lastCheckIn={lastCheckIn}
-					handleUpdate={data => {}}
-					onSuccess={() => {}}
-				/>
-				<CheckInList lastData={lastCheckIn} />
-			</Modal>
-		</>
-	);
+  useEffect(() => {
+    dispatch(getAttendanceLastCheckIn());
+  }, [isOpen]);
+  useEffect(() => {
+    if (success) setIsOpen(false);
+  }, [success]);
+  return (
+    <>
+      <div className="flex items-center text-xs">
+        <div>{isMobile ? moment().format("lll") : moment().format("LLLL")}</div>
+        <div className="cursor-pointer" onClick={() => setIsOpen(true)}>
+          <img
+            className={
+              "checkInBtn ml-2 " +
+              (lastCheckIn && lastCheckIn.type === CHECK_IN.IN
+                ? "blinkInfoRed"
+                : "blinkInfo")
+            }
+            src={
+              lastCheckIn && lastCheckIn.type === CHECK_IN.IN
+                ? ClockOut
+                : ClockIn
+            }
+            alt=""
+          />
+        </div>
+      </div>
+      <Modal
+        title={null}
+        centered
+        visible={isOpen}
+        footer={null}
+        closeIcon={null}
+        className="checkInModal"
+        onOk={() => setIsOpen(false)}
+        onCancel={() => setIsOpen(false)}
+        width="445px"
+      >
+        <CheckInHeader />
+        <CheckInCard
+          lastCheckIn={lastCheckIn}
+          handleUpdate={(data) => {}}
+          onSuccess={() => {}}
+        />
+        <CheckInList lastData={lastCheckIn} />
+      </Modal>
+    </>
+  );
 };
 export default CheckIn;

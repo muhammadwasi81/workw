84a0a006cbc27c6bdf29cf23ed323c0ea13da28b
Sanjana Bajaj <sanjana@miletap.com>
2023-03-11 16:29:53 +0500

    working on approvers

--- src/main/features/warning/view/DetailCard.js
+++ src/main/features/warning/view/DetailCard.js
@@ -11,198 +11,192 @@ import DefaultAttachment from "../../../../content/NewContent/warning/warningsDe
 import Avatar from "../../../sharedComponents/Avatar/avatar";
 import moment from "moment";
 import {
-	ItemContent,
-	ItemHeader,
+  ItemContent,
+  ItemHeader,
 } from "../../../sharedComponents/Card/CardStyle";
 import RemarksApproval from "../../../sharedComponents/AppComponents/Approvals/view";
 import {
-	ApprovalsModule,
-	ApprovalStatus,
+  ApprovalsModule,
+  ApprovalStatus,
 } from "../../../sharedComponents/AppComponents/Approvals/enums";
 import { cancelWarning, GetWarningById } from "../store/actions";
 import { useDispatch } from "react-redux";
 import ConfirmationRemarkModal from "../../../sharedComponents/ConfirmationRemarkModal/ConfirmationRemarkModal";
 
 function DetaileCard(props) {
-	const dispatch = useDispatch();
-	const { userLanguage } = useContext(LanguageChangeContext);
-	const {
-		warningDictionary,
-	} = warningDictionaryList[userLanguage];
-	const [updatedStatus, setUpdatedStatus] = useState(null);
-	const { user } = useSelector(state => state.userSlice);
-	const [isOpen, setIsOpen] = useState(false)
+  const dispatch = useDispatch();
+  const { userLanguage } = useContext(LanguageChangeContext);
+  const { warningDictionary } = warningDictionaryList[userLanguage];
+  const [updatedStatus, setUpdatedStatus] = useState(null);
+  const { user } = useSelector((state) => state.userSlice);
+  const [isOpen, setIsOpen] = useState(false);
 
-	let userId = user.id;
+  let userId = user.id;
 
-	const { warningDetail, loadingData } = useSelector(
-		state => state.warningSlice
-	);
-	let {
-		InProcess,
-		Approved,
-		Declined,
-		Resend,
-		Inactive,
-		NotRequired,
-		Cancelled,
-		ApprovalRequired,
-		Hold,
-		NoStatus,
-	} = ApprovalStatus;
+  const { warningDetail, loadingData } = useSelector(
+    (state) => state.warningSlice
+  );
+  console.log(warningDetail, "warning detaill");
+  let {
+    InProcess,
+    Approved,
+    Declined,
+    Resend,
+    Inactive,
+    NotRequired,
+    Cancelled,
+    ApprovalRequired,
+    Hold,
+    NoStatus,
+  } = ApprovalStatus;
 
-	useEffect(() => {
-		console.log("BEFORE");
-		props.id && dispatch(GetWarningById(props.id));
-		console.log("AFTER");
-	}, [props.id]);
+  useEffect(() => {
+    props.id && dispatch(GetWarningById(props.id));
+  }, [props.id]);
 
-	const {
-		creator,
-		description,
-		image = DefaultAttachment,
-		category,
-		status,
-		createDate,
-		members = [],
-		approvers,
-		referenceNo,
-		id,
-	} = warningDetail === null ? "" : warningDetail;
+  const {
+    creator,
+    description,
+    image = DefaultAttachment,
+    category,
+    status,
+    createDate,
+    members = [],
+    approvers,
+    referenceNo,
+    id,
+  } = warningDetail === null ? "" : warningDetail;
 
-	const isTablet = useMediaQuery({ maxWidth: 800 });
+  const isTablet = useMediaQuery({ maxWidth: 800 });
 
-	const handleCancel = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		setIsOpen(true)
-	};
-	
-	  const onClose = () => {
-		setIsOpen(false);
-	  };
-	
-	  const onFinish = (values) => { 
-		let id = warningDetail.id;
-		let reason = values.remarks
-		setIsOpen(false);
-		// dispatch(cancelReward({ id: id, reason: reason }));
-	  }
+  const handleCancel = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsOpen(true);
+  };
 
-	if (loadingData) return <Skeleton />;
+  const onClose = () => {
+    setIsOpen(false);
+  };
 
-	return (
-		<>
-			{warningDetail && warningDetail.id && (
-				<div className="detailedCard" id={props.id}>
-					<ItemHeader>
-						<div className="left">
-							<UserInfo
-								avatarSrc={creator && creator.image}
-								name={creator && creator.name}
-								Subline={
-									<SublineDesigWithTime
-										designation={
-											creator && creator.designation
-												? creator.designation
-												: ""
-										}
-										time={moment(createDate).fromNow()}
-									/>
-								}
-							/>
-						</div>
-						<div className="right">
-							<Tag className="IdTag">{referenceNo}</Tag>
-							<StatusTag status={status}></StatusTag>
-							{userId === creator && creator.id ? (
-								status != Declined &&
-								status != Resend &&
-								status != Approved ? (
-									<Button
-										className="ThemeBtn"
-										onClick={e => handleCancel(e, id)}
-									>
-										Cancel
-									</Button>
-								) : (
-									""
-								)
-							) : (
-								""
-							)}
-						</div>
-					</ItemHeader>
-					<ItemContent className="flex">
-						<div className="description w-full">
-							<p>{description}</p>
-						</div>
-					</ItemContent>
-					<div className="cardSections">
-						<div className="cardSectionItem">
-							<div className="cardSection__title">
-								{warningDictionary.category}
-							</div>
-							<div className="cardSection__body">
-								<Tag className="IdTag">
-									{category ? category : "Default Category"}
-								</Tag>
-							</div>
-						</div>
-						<div className="cardSectionItem">
-							<div className="cardSection__title">
-								{warningDictionary.warningTo}
-							</div>
-							<div className="cardSection__body">
-								{members && (
-									<Avatar
-										isAvatarGroup={true}
-										isTag={false}
-										heading={"Members"}
-										membersData={members}
-										text={"Members"}
-										image={
-											"https://joeschmoe.io/api/v1/random"
-										}
-									/>
-								)}
-							</div>
-						</div>
-						<div className="cardSectionItem">
-							<div className="cardSection__title">
-								{warningDictionary.approvers}
-							</div>
-							<div className="cardSection__body">
-								{approvers && (
-									<Avatar
-										isAvatarGroup={true}
-										isTag={false}
-										heading={"Approvers"}
-										membersData={approvers}
-										text={"Approvers"}
-										image={
-											"https://joeschmoe.io/api/v1/random"
-										}
-									/>
-								)}
-							</div>
-						</div>
-					</div>
-					<RemarksApproval
-						module={ApprovalsModule.WarningApproval}
-						status={status}
-						onStatusChanged={statusChanged => {
-							setUpdatedStatus(statusChanged);
-							console.log(statusChanged);
-						}}
-						data={approvers}
-						title="Approvers"
-					/>
-				</div>
-			)}
-			<ConfirmationRemarkModal isOpen={isOpen} onCancel={onClose} onFinish={onFinish} />
-		</>
-	);
+  const onFinish = (values) => {
+    let id = warningDetail.id;
+    let reason = values.remarks;
+    setIsOpen(false);
+    // dispatch(cancelReward({ id: id, reason: reason }));
+  };
+
+  if (loadingData) return <Skeleton />;
+
+  return (
+    <>
+      {warningDetail && warningDetail.id && (
+        <div className="detailedCard" id={props.id}>
+          <ItemHeader>
+            <div className="left">
+              <UserInfo
+                avatarSrc={creator && creator.image}
+                name={creator && creator.name}
+                Subline={
+                  <SublineDesigWithTime
+                    designation={
+                      creator && creator.designation ? creator.designation : ""
+                    }
+                    time={moment(createDate).fromNow()}
+                  />
+                }
+              />
+            </div>
+            <div className="right">
+              <Tag className="IdTag">{referenceNo}</Tag>
+              <StatusTag status={status}></StatusTag>
+              {userId === creator && creator.id ? (
+                status != Declined && status != Resend && status != Approved ? (
+                  <Button
+                    className="ThemeBtn"
+                    onClick={(e) => handleCancel(e, id)}
+                  >
+                    Cancel
+                  </Button>
+                ) : (
+                  ""
+                )
+              ) : (
+                ""
+              )}
+            </div>
+          </ItemHeader>
+          <ItemContent className="flex">
+            <div className="description w-full">
+              <p>{description}</p>
+            </div>
+          </ItemContent>
+          <div className="cardSections">
+            <div className="cardSectionItem">
+              <div className="cardSection__title">
+                {warningDictionary.category}
+              </div>
+              <div className="cardSection__body">
+                <Tag className="IdTag">
+                  {category ? category : "Default Category"}
+                </Tag>
+              </div>
+            </div>
+            <div className="cardSectionItem">
+              <div className="cardSection__title">
+                {warningDictionary.warningTo}
+              </div>
+              <div className="cardSection__body">
+                {members && (
+                  <Avatar
+                    isAvatarGroup={true}
+                    isTag={false}
+                    heading={"Members"}
+                    membersData={members}
+                    text={"Members"}
+                    image={"https://joeschmoe.io/api/v1/random"}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="cardSectionItem">
+              <div className="cardSection__title">
+                {warningDictionary.approvers}
+              </div>
+              <div className="cardSection__body">
+                {approvers && (
+                  <Avatar
+                    isAvatarGroup={true}
+                    isTag={false}
+                    heading={"Approvers"}
+                    membersData={approvers}
+                    text={"Approvers"}
+                    image={"https://joeschmoe.io/api/v1/random"}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+          <RemarksApproval
+            module={ApprovalsModule.WarningApproval}
+            referenceId={warningDetail.id}
+            status={status}
+            onStatusChanged={(statusChanged) => {
+              setUpdatedStatus(statusChanged);
+              console.log(statusChanged);
+            }}
+            data={approvers}
+            title="Approvers"
+          />
+        </div>
+      )}
+      <ConfirmationRemarkModal
+        isOpen={isOpen}
+        onCancel={onClose}
+        onFinish={onFinish}
+      />
+    </>
+  );
 }
 
 export default DetaileCard;

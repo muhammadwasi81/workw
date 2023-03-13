895f0ee58aaafe7b7c98cb20610732c7cc421041
Humayoun shah <humayun@miletap.com>
2023-03-13 17:25:33 +0500

    loader schedule

--- src/main/features/schedule/UI/ScheduleCard.js
+++ src/main/features/schedule/UI/ScheduleCard.js
@@ -5,6 +5,7 @@ import { getAllSchedule } from "../store/action";
 import ScheduleCardDetail from "./ScheduleCardDetail";
 import { defaultUiid } from "../../../../utils/Shared/enums/enums";
 import Scroll from "../../../sharedComponents/ScrollSelect/infinteScoll";
+import { Skeleton } from "antd";
 
 function ScheduleCard({ sheduleType = "", setScheduleData = () => {} }) {
   const schedules = useSelector((state) => state.scheduleSlice.schedules);
@@ -103,37 +104,49 @@ function ScheduleCard({ sheduleType = "", setScheduleData = () => {} }) {
     });
   };
   return (
-    <div
-      style={{
-        height: "70vh",
-      }}
-    >
-      <Scroll
-        isLoading={loading}
-        data={schedules}
-        fetchMoreData={(pageNo) => {
-          setPageNo(pageNo);
-        }}
-        loader={<>Loading schedules...</>}
-        endMessage={"No data..."}
-      >
-        {schedules?.map((schedule) => (
-          <div
-            className="rounded-lg p-3 bg-white  flex flex-col gap-3 mb-2 hover:border-primary-color hover:border transition-all border"
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              handleScheduleTypeData(schedule);
-            }}
-          >
-            <div className="flex flex-col">
-              {/* <p className="text-[#757d86]">SAT,Jul 14,2022</p> */}
-              <ScheduleCardDetail schedule={schedule} />
+    <>
+      {loading ? (
+        <Skeleton active />
+      ) : (
+        <>
+          {schedules.length > 0 ? (
+            <div
+              style={{
+                height: "70vh",
+              }}
+            >
+              <Scroll
+                isLoading={loading}
+                data={schedules}
+                fetchMoreData={(pageNo) => {
+                  setPageNo(pageNo);
+                }}
+                loader={<>Loading schedules...</>}
+                endMessage={"No data..."}
+              >
+                {schedules?.map((schedule) => (
+                  <div
+                    className="rounded-lg p-3 bg-white  flex flex-col gap-3 mb-2 hover:border-primary-color hover:border transition-all border"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      handleScheduleTypeData(schedule);
+                    }}
+                  >
+                    <div className="flex flex-col">
+                      {/* <p className="text-[#757d86]">SAT,Jul 14,2022</p> */}
+                      <ScheduleCardDetail schedule={schedule} />
+                    </div>
+                  </div>
+                ))}
+              </Scroll>
             </div>
-          </div>
-        ))}
-      </Scroll>
-    </div>
+          ) : (
+            <div>No Data</div>
+          )}
+        </>
+      )}
+    </>
   );
 }
 

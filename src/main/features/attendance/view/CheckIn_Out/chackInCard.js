a42f6dd619fd4c8f30ededce86bed45695a496f7
Sanjana Bajaj <sanjana@miletap.com>
2023-03-27 11:32:17 +0500

    checkin and checkout done

--- src/main/features/attendance/view/CheckIn_Out/chackInCard.js
+++ src/main/features/attendance/view/CheckIn_Out/chackInCard.js
@@ -1,3 +1,4 @@
+import { Modal, Popconfirm } from "antd";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
@@ -12,7 +13,8 @@ const CheckInCard = ({ lastCheckIn }) => {
   const [rattingVal, setRattingVal] = useState(3);
   const [noteVal, setNoteVal] = useState("");
   const [myLocation, setMyLocation] = useState(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  //   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isPopconfirmVisible, setIsPopconfirmVisible] = useState(false);
 
   const { CHECK_IN } = ATTENDANCE_ENUM;
 
@@ -23,6 +25,7 @@ const CheckInCard = ({ lastCheckIn }) => {
   }, []);
 
   const handleSubmit = () => {
+    console.log("lllll");
     let payload = {
       lat: myLocation ? myLocation.coords.latitude : 0,
       lng: myLocation ? myLocation.coords.longitude : 0,
@@ -35,54 +38,78 @@ const CheckInCard = ({ lastCheckIn }) => {
           : CHECK_IN.IN,
     };
     dispatch(addAttendanceCheckIn(payload));
+    setIsPopconfirmVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsPopconfirmVisible(false);
   };
   return (
-    <div className="CheckInCard">
-      {/* {isLoading && <div className='checkInSpinnerHold' ><Spinner /></div>} */}
-      <div className="firstCont">
-        <div className="headingCont">
-          <div></div>
-          <div className="checkInTime dateSpace">{moment().format("LTS")}</div>
-          <div className="checkInDate">
-            {moment().format("dddd MMM Do YYYY")}
+    <>
+      <div className="CheckInCard">
+        {/* {isLoading && <div className='checkInSpinnerHold' ><Spinner /></div>} */}
+        <div className="firstCont">
+          <div className="headingCont">
+            <div></div>
+            <div className="checkInTime dateSpace">
+              {moment().format("LTS")}
+            </div>
+            <div className="checkInDate">
+              {moment().format("dddd MMM Do YYYY")}
+            </div>
           </div>
-        </div>
-        {
-          <div
-            className={
-              "radius50 " +
-              (lastCheckIn && lastCheckIn.type === CHECK_IN.IN
-                ? "blinkInfoRed"
-                : "blinkInfo")
+          <Popconfirm
+            title={
+              lastCheckIn && lastCheckIn.type === CHECK_IN.IN
+                ? "Are You Sure to CheckIn?"
+                : "Are You Sure to CheckOut?"
             }
+            // description="Are You sure to ChceckIn"
+            onConfirm={handleSubmit}
+            onCancel={handleCancel}
+            okText="Yes"
+            cancelText="No"
+            visible={isPopconfirmVisible}
           >
-            <img
-              src={
-                lastCheckIn && lastCheckIn.type === CHECK_IN.IN
-                  ? ClockOut
-                  : ClockIn
-              }
-              alt=""
-              onClick={handleSubmit}
+            {
+              <div
+                className={
+                  "radius50 " +
+                  (lastCheckIn && lastCheckIn.type === CHECK_IN.IN)
+                    ? "blinkInfoRed"
+                    : "blinkInfo"
+                }
+                //   onClick={() => setIsModalVisible(true)}
+              >
+                <img
+                  src={
+                    lastCheckIn && lastCheckIn.type === CHECK_IN.IN
+                      ? ClockOut
+                      : ClockIn
+                  }
+                  alt=""
+                  onClick={() => setIsPopconfirmVisible(true)}
+                />
+              </div>
+            }
+          </Popconfirm>
+        </div>
+
+        <div className="mt-2">
+          <FaceRating handleChange={(value) => setRattingVal(value)} />
+        </div>
+
+        <div className="checkInNote">
+          <div className="checkInNoteTitle">Note</div>
+          <div className="checkInInputCont">
+            <input
+              placeholder="I was late for the cause of traffic."
+              onChange={(e) => setNoteVal(e.target.value)}
             />
           </div>
-        }
-      </div>
-
-      <div className="mt-2">
-        <FaceRating handleChange={(value) => setRattingVal(value)} />
-      </div>
-
-      <div className="checkInNote">
-        <div className="checkInNoteTitle">Note</div>
-        <div className="checkInInputCont">
-          <input
-            placeholder="I was late for the cause of traffic."
-            onChange={(e) => setNoteVal(e.target.value)}
-          />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default CheckInCard;
